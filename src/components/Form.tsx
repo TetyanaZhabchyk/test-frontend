79f870cd169e1b02069d82e35b56d89934b89ed6
Chouteau <tetyana.chouteau@passculture.app>
2023-02-20 13:31:54 +0100

    Typage des Cat, Date, Desc

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,23 +1,32 @@
 import React from "react";
 import "./Form.css";
-import { useState, ChangeEvent, FormEvent } from "react";
+import { useState } from "react";
 
 function Form() {
   const [titre, setTitre] = useState("");
-  //const [cat, setCat] = useState("");
-  //const [date, setDate] = useState("");
-  //const [desc, setDesc] = useState("");
+  const [cat, setCat] = useState("");
+  const [date, setDate] = useState("");
+  const [desc, setDesc] = useState("");
 
   const handleSubmit = (event: React.FormEvent) => {
     console.log("titre: " + titre);
-    //console.log("date: " + date);
-    //console.log("desc: " + desc);
-    //console.log("cat: " + cat);
+    console.log("date: " + date);
+    console.log("desc: " + desc);
+    console.log("cat: " + cat);
     event.preventDefault();
   };
 
   const handleTitreChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTitre(event.currentTarget.value);
+  };
+  const handleCatChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCat(event.currentTarget.value);
+  };
+  const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDate(event.currentTarget.value);
+  };
+  const handleDescChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDesc(event.currentTarget.value);
   };
 
   //onChange
@@ -28,11 +37,11 @@ function Form() {
         <label htmlFor="titre">Titre</label>
         <input type="text" id="titre" onChange={handleTitreChange}></input>
         <label htmlFor="cat">Catégorie</label>
-        <input type="text" id="cat"></input>
+        <input type="text" id="cat" onChange={handleCatChange}></input>
         <label htmlFor="date">Date</label>
-        <input type="date" id="date"></input>
+        <input type="date" id="date" onChange={handleDateChange}></input>
         <label htmlFor="desc">Description</label>
-        <input type="text" id="desc"></input>
+        <input type="text" id="desc" onChange={handleDescChange}></input>
         <button type="submit">Valider</button>
         <a href="/">Retourner à l'accueil</a>
       </form>
